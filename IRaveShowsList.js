03e67a3774047d6be366a210c4ab389c90bad57b
xtrm0 <afonso.tfaria21@gmail.com>
2018-04-30 14:40:25 +0100

    cenas

--- IRaveShowsList.js
+++ IRaveShowsList.js
@@ -2,26 +2,32 @@ var scheduledata = {
 
 	days:[
 	{	
-		day:'1',
+		name: 'Day 1',
+		day:'abcda',
 		stages:[
 		{
+			name:'Stage A',
 			stage:'A',
 			concerts:[{time:"18:30", artist:"Band1"}, {time:"20:30", artist:"Band2"}, {time:"22:30", artist:"Band3"}, {time:"00:30", artist:"Band4"}]
 		},
 		{
+			name:'Stage B',
 			stage:'B',
 			concerts:[{time:"19:30", artist:"Band1"}, {time:"21:00", artist:"Band2"}, {time:"22:30", artist:"Band3"}, {time:"00:00", artist:"Band4"}]
 		}
 		]
 	},
 	{	
-		day:'2',
+		name: 'Day 2',
+		day:'cbhdsabcd',
 		stages:[
 		{
+			name:'Stage A',
 			stage:'A',
 			concerts:[{time:"18:30", artist:"Band5"}, {time:"20:30", artist:"Band6"}, {time:"22:30", artist:"Band7"}, {time:"00:30", artist:"Band8"}]
 		},
 		{	
+			name:'Stage B',
 			stage:'B',
 			concerts:[{time:"19:30", artist:"Band5"}, {time:"21:00", artist:"Band6"}, {time:"22:30", artist:"Band7"}, {time:"00:00", artist:"Band8"}]
 		}
