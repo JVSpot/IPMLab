8f26dacf6399d7428cf3c5c1c545cfebe573d278
MisterPac <38846047+MisterPac123@users.noreply.github.com>
2018-05-13 22:20:50 +0100

    FoodStands

--- IRaveFoodStandsList.js
+++ IRaveFoodStandsList.js
@@ -3,28 +3,28 @@ var foodstandsData = {
 	stands:[
 	{	
 		name: 'Stand1',
-		order_types:[
+		item_types:[
 		{
 			name:'Menu',
-			orders:[{name:'Hamburger menu', descrition:'Hamburger + Coke + Icecream', price:'7.50'}, 
-					{name:'Steak menu', descrition:'Steak + Chips + Beer + Coffee', price:'8.50'},
-					{name:'Nuggets menu', descrition:'Nuggets + Coke + Icecream', price:'6.50'} 
+			items:[{name:'Hamburger menu', descrition:'Hamburger + Coke + Icecream', price:7.50}, 
+					{name:'Steak menu', descrition:'Steak + Chips + Beer + Coffee', price:8.50},
+					{name:'Nuggets menu', descrition:'Nuggets + Coke + Icecream', price:6.50} 
 			]
 		},
 		{
 			name:'Eats',
-			orders:[{name:'Hamburger', price:'4.30'},
-					{name:'Steak', price:'5.00'},
-					{name:'Nuggets', price:'3.00'}
+			items:[{name:'Hamburger', price:4.30},
+					{name:'Steak', price:5.00},
+					{name:'Nuggets', price:3.00}
 			]
 		},
 		{
 			name:'Drinks',
-			orders:[{name:'Beer', price:'0.50'},
-					{name:'Water', price:'0.50'},
-					{name:'Orange juice', price:'1.00'},
-					{name:'Soda', price:'0.75'},
-					{name:'Coffee', price:'0.80'}
+			items:[{name:'Beer', price:0.50},
+					{name:'Water', price:0.50},
+					{name:'Orange juice', price:1.00},
+					{name:'Soda', price:0.75},
+					{name:'Coffee', price:0.80}
 			]
 		}
 
@@ -32,28 +32,28 @@ var foodstandsData = {
 	},
 	{	
 		name: 'Stand2',
-		order_types:[
+		item_types:[
 		{
 			name:'Menu',
-			orders:[{name:'Kebbab menu', descrition:'Kebbab + Coke + Jelly', price:'4.50'}, 
-					{name:'Steak menu', descrition:'Steak + Chips + Beer + Coffee', price:'7.95'},
-					{name:'Soup menu', descrition:'Soup + Coke + Icecream', price:'6.50'}
+			items:[{name:'Kebbab menu', descrition:'Kebbab + Coke + Jelly', price:4.50}, 
+					{name:'Steak menu', descrition:'Steak + Chips + Beer + Coffee', price:7.95},
+					{name:'Soup menu', descrition:'Soup + Coke + Icecream', price:6.50}
 			]
 		},
 		{
 			name:'Eats',
-			orders:[{name:'Kebbab', price:'2.30'},
-					{name:'Steak', price:'5.00'},
-					{name:'Nuggets', price:'3.00'}
+			items:[{name:'Kebbab', price:2.30},
+					{name:'Steak', price:5.00},
+					{name:'Nuggets', price:3.00}
 			]
 		},
 		{
 			name:'Drinks',
-			orders:[{name:'Beer', price:'0.50'},
-					{name:'Water', price:'0.50'},
-					{name:'Orange juice', price:'1.00'},
-					{name:'Soda', price:'0.75'},
-					{name:'Coffee', price:'0.80'}
+			items:[{name:'Beer', price:0.50},
+					{name:'Water', price:0.50},
+					{name:'Orange juice', price:1.00},
+					{name:'Soda', price:0.75},
+					{name:'Coffee', price:0.80}
 			]
 		}
 		]
