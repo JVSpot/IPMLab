b12770961f69f53a9adbdb6c6711472ef09c3ec6
MisterPac <38846047+MisterPac123@users.noreply.github.com>
2018-05-14 15:55:23 +0100

    Bandas

--- IRaveShowsList.js
+++ IRaveShowsList.js
@@ -8,12 +8,12 @@ var scheduledata = {
 		{
 			name:'Stage A',
 			stage:'A',
-			concerts:[{time:"18:30", artist:"My Chemical Romance", going: ["a", "b"], notification: false}, {time:"20:30", artist:"Band2", going: [], notification: false}, {time:"22:30", artist:"Band3", going: [], notification: false}, {time:"00:30", artist:"Band4", going: [], notification: false}]
+			concerts:[{time:"18:30", artist:"My Chemical Romance", going: ["a", "b"], notification: false}, {time:"20:30", artist:"Muse", going: [], notification: false}, {time:"22:30", artist:"Pond", going: [], notification: false}, {time:"00:30", artist:"Strokes", going: [], notification: false}]
 		},
 		{
 			name:'Stage B',
 			stage:'B',
-			concerts:[{time:"19:30", artist:"Band1", going: [], notification: false}, {time:"21:00", artist:"Band2", going: [], notification: false}, {time:"22:30", artist:"Band3", going: [], notification: false}, {time:"00:00", artist:"Band4", going: [], notification: false}]
+			concerts:[{time:"19:30", artist:"Modernos", going: [], notification: false}, {time:"21:00", artist:"Andrage", going: [], notification: false}, {time:"22:30", artist:"Ty Segall", going: [], notification: false}, {time:"00:00", artist:"Foo Fighters", going: [], notification: false}]
 		}
 		]
 	},
@@ -24,12 +24,12 @@ var scheduledata = {
 		{
 			name:'Stage A',
 			stage:'A',
-			concerts:[{time:"18:30", artist:"Band5", going: [], notification: false}, {time:"20:30", artist:"Band6", going: [], notification: false}, {time:"22:30", artist:"Band7", going: [], notification: false}, {time:"00:30", artist:"Band8", going: [], notification: false}]
+			concerts:[{time:"18:30", artist:"King Gizzard and the Lizard Wizard", going: [], notification: false}, {time:"20:30", artist:"Pink Floyd", going: [], notification: false}, {time:"22:30", artist:"The Doors", going: [], notification: false}, {time:"00:30", artist:"Nirvana", going: [], notification: false}]
 		},
 		{	
 			name:'Stage B',
 			stage:'B',
-			concerts:[{time:"19:30", artist:"Band5", going: [], notification: false}, {time:"21:00", artist:"Band6", going: [], notification: false}, {time:"22:30", artist:"Band7", going: [], notification: false}, {time:"00:00", artist:"Band8", going: [], notification: false}]
+			concerts:[{time:"19:30", artist:"Hinds", going: [], notification: false}, {time:"21:00", artist:"Modest Mouse", going: [], notification: false}, {time:"22:30", artist:"The Growlers", going: [], notification: false}, {time:"00:00", artist:"New Order", going: [], notification: false}]
 		}
 		]
 	}
